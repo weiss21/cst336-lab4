149a09c64a193ae5c63ba4dccde5a888fc9b9531
waisalirobleh <waisalirobleh@gmail.com>
2019-07-21 08:40:57 -0400

    heroku fix

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require("express"); //imports Express Library
 const app = express(); //variable "app" to access methods.
-app.engine("html", require("ejs").renderFile);
+app.engine('html', require('ejs').renderFile);
 app.use(express.static("public"));
 
 
-//server listener
-app.listen("8081", "0.0.0.0", function(){
-  console.log("Express Server is Running...")
+//server listener original example
+//app.listen("8081", "0.0.0.0", function(){
+//  console.log("Express Server is Running...")
+//});
+
+
+ // Heroku update server listener
+app.listen(process.env.PORT, process.env.IP, function(){
+  console.log("Express Server is Running. . . ")
 });
 
-//Routes for Server
 
 app.get("/", function(req, res) {
     res.render("index.html");
@@ -22,6 +27,3 @@ app.get("/venus", function(req, res) {
     res.render("venus.html");
 })
 
-app.get("/", function(req, res) {
-    res.render("index.html");
-});
